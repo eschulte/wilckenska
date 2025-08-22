eec89bed0d7dbbbf0823887fbf95bbe062c25912
Eric Schulte <schulte.eric@gmail.com>
2025-08-22 11:20:42 -0400

    Path prefix

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,12 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
 import relativeLinks from "./_config/relative-links.js";
 
 export default async function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("favicon.ico");
+
+  eleventyConfig.addPlugin(HtmlBasePlugin);
 
   eleventyConfig.addLiquidFilter("fixDate", function(value){
     value.setTime(value.getTime() + (5*60*60*1000));
